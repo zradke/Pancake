ef486d30e907009e7bc39470702e1952ad11f8af
Zach Radke <zach.radke@gmail.com>
2017-11-27 23:31:51 -0800

    Fix failing test due to asynchronous observations

--- PancakeTests/BatchCacheTests.swift
+++ PancakeTests/BatchCacheTests.swift
@@ -13,15 +13,19 @@ class BatchCacheTests: XCTestCase {
     }
 
     func testBatchUpdateCoalescingObservations() {
-        // GIVEN: A model inserted in the cache and observed
+        // GIVEN: A model inserted in the cache
         let model = SimpleModel(identifier: "A", counter: 1)
         cache.set(model)
+        let cached = cache.cached(model)
 
+        // GIVEN: The cached model is observed
         var observationCount = 0
         var observedModel: SimpleModel?
-        let disposable = cache.observe(model) {
+        let expectation = self.expectation(description: "Observer notified")
+        let disposable = cached.observe {
             observationCount += 1
             observedModel = $0
+            expectation.fulfill()
         }
         _ = disposable
 
@@ -37,26 +41,33 @@ class BatchCacheTests: XCTestCase {
             cache.set(updated)
         }
 
-        // EXPECT: The value to be changed in the cache and the observer to be notified once
+        // EXPECT: The value to be changed in the cache
         let expected = SimpleModel(identifier: "A", counter: 3)
         XCTAssertEqual(cache.get(model.identifier), expected)
+
+        // EXPECT: The observer to be notified once
+        waitForExpectations(timeout: 2, handler: nil)
         XCTAssertEqual(observedModel, expected)
         XCTAssertEqual(observationCount, 1)
     }
 
     func testBatchUpdateCoalescingRelatedObservations() {
-        // GIVEN: Related models inserted in the cache and observed
+        // GIVEN: Related models inserted in the cache
         let grandparent = RelatedModel(identifier: "C", label: nil, parent: nil) // NOTE: This will be inserted in the batch updates
         let parent = RelatedModel(identifier: "A", label: nil, parent: nil)
         let child = RelatedModel(identifier: "B", label: nil, parent: Related(parent))
         cache.set(parent)
         cache.set(child)
 
+        // GIVEN: The cached child is observed
+        let cachedChild = cache.cached(child)
         var observationCount = 0
         var observedModel: RelatedModel?
-        let disposable = cache.observe(child) {
+        let expectation = self.expectation(description: "Observer notified")
+        let disposable = cachedChild.observe {
             observationCount += 1
             observedModel = $0
+            expectation.fulfill()
         }
         _ = disposable
 
@@ -74,24 +85,30 @@ class BatchCacheTests: XCTestCase {
             cache.set(updated)
         }
 
-        // EXPECT: The value to be changed in the cache and the observer to be notified once
+        // EXPECT: The value to be changed in the cache
         XCTAssertEqual(cache.get(parent.identifier), RelatedModel(identifier: "A", label: "New", parent: Related(grandparent)))
         XCTAssertEqual(cache.get(grandparent.identifier), grandparent)
+
+        // EXPECT: The observer to be notified once
+        waitForExpectations(timeout: 2, handler: nil)
         XCTAssertEqual(observedModel, child)
         XCTAssertEqual(observationCount, 1)
     }
 
     func testRetreiveCachedValueWithinObservationDuringBatchUpdate() {
-        // GIVEN: A model inserted in the cache and uses a cached value inside the observation
+        // GIVEN: A model inserted in the cache
         let model = SimpleModel(identifier: "A", counter: 1)
         cache.set(model)
         let cached = cache.cached(model)
 
+        // GIVEN: The cached model is observed and a cached value is retreived in the observation
         var observationCount = 0
         var observedModel: SimpleModel?
+        let expectation = self.expectation(description: "Observer notified")
         let disposable = cached.observe { [weak cached] (_) in
             observationCount += 1
             observedModel = cached?.value
+            expectation.fulfill()
         }
         _ = disposable
 
@@ -107,9 +124,12 @@ class BatchCacheTests: XCTestCase {
             cache.set(updated)
         }
 
-        // EXPECT: The value to be changed in the cache and the observer to be notified once
+        // EXPECT: The value to be changed in the cache
         let expected = SimpleModel(identifier: "A", counter: 3)
         XCTAssertEqual(cache.get(model.identifier), expected)
+
+        // EXPECT: The observer to be notified once
+        waitForExpectations(timeout: 2, handler: nil)
         XCTAssertEqual(observedModel, expected)
         XCTAssertEqual(observationCount, 1)
     }
